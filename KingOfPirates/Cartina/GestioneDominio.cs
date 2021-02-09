3a3b64479377b35caa381837e2ae430b459f03ff
EatPastaKillFasta <78098716+EatPastaKillFasta@users.noreply.github.com>
2021-02-09 19:57:27 +0100

    gestionedomino/negozio

--- KingOfPirates/Cartina/GestioneDominio.cs
+++ KingOfPirates/Cartina/GestioneDominio.cs
@@ -11,11 +11,24 @@ namespace KingOfPirates.Cartina
         private int tasse; //dobloni per turno
         private int cassaDobloni;
         private int cassaRubini;
+        private int taglieMercantile;
+        private int taglieCaravella;
+        private int taglieFregata;
 
         public GestioneDominio()
         {
-
+            cassaDobloni = 0;
+            cassaRubini = 0;
+            taglieMercantile = 0;
+            taglieCaravella = 0;
+            taglieFregata = 0;
         }
+
+        public int TaglieMercantile { get => taglieMercantile; set => taglieMercantile = value; }
+
+        public int TaglieCaravella { get => taglieCaravella; set => taglieCaravella = value; }
+
+        public int TaglieFregata { get => taglieFregata; set => taglieFregata = value; }
 
         public void AddTasse(int val)
         {
--- KingOfPirates/Nassau/Negozio.cs
+++ KingOfPirates/Nassau/Negozio.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KingOfPirates.Cartina;
+using KingOfPirates.Missioni.Navi;
 
 using System.Drawing; //Per le Bitmap
 
@@ -15,11 +17,31 @@ namespace KingOfPirates.Nassau
 
         public Negozio(Bitmap immagine_) : base("Negozio", immagine_)
         {
-
+            livelloNegozio = 0;
         }
 
-        //TODO: public void RiscattaTaglie(Player p){}
-        //TODO: public void AcquistaBevandaDeterminazione(Player p){}
+        public void RiscattaTaglie(GestioneDominio gest){
+            int taglieMerca = gest.TaglieMercantile;            //assegnazione del numero di taglie per tipo
+            int taglieCarav = gest.TaglieCaravella;
+            int taglieFrega = gest.TaglieFregata;
+
+            int valoreMerca = 1;                                //valore per tipo di taglia
+            int valoreCarav = 1;
+            int valoreFrega = 1;
+
+            gest.AddDobloni(taglieMerca * valoreMerca);         //riscossione taglia, aggiunta dobloni alla cassa
+            gest.AddDobloni(taglieCarav * valoreCarav);
+            gest.AddDobloni(taglieFrega * valoreFrega);
+
+            gest.TaglieMercantile = 0;                          //reset nTaglie
+            gest.TaglieCaravella = 0;
+            gest.TaglieFregata = 0;
+        }
+
+        public void AcquistaBevandaDeterminazione(NaveGiocatore player){
+            
+        }
+
         //TODO: public void AcquistaRum(Player p){}
         //TODO: public void AcquistaAntiubriachezza(Player p){}
         //TODO: public void AcquistaAssiLegno(Player p){}
