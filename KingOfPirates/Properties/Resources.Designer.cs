b81a9ce8bc31935884d40eab0b4ddbabac4eff92
iuri <iuricube@gmail.com>
2021-02-18 09:17:35 +0100

    Aggiustato Resources.resx, inclusi alcuni file

--- KingOfPirates/Properties/Resources.Designer.cs
+++ KingOfPirates/Properties/Resources.Designer.cs
@@ -233,6 +233,16 @@ namespace KingOfPirates.Properties {
         /// <summary>
         ///   Cerca una risorsa localizzata di tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap mare {
+            get {
+                object obj = ResourceManager.GetObject("mare", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Cerca una risorsa localizzata di tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap nascondi {
             get {
                 object obj = ResourceManager.GetObject("nascondi", resourceCulture);
